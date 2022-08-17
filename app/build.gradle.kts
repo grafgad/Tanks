5d1d20dc5aa4c8730282184a6e2c553d72080b1f
grafgad <grafgad@mail.ru>
2022-08-17 13:33:46 +0300

    переход на build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,126 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+    id("kotlin-parcelize")
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        applicationId = "com.example.tanks"
+        minSdk = 23
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes.onEach {
+        val properties = Properties()
+        properties.load(project.rootProject.file("apikey.properties").inputStream())
+        val appId = properties.getProperty("APPLICATION_ID", "")
+        it.buildConfigField("String", "APPLICATION_ID", appId)
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        getByName("debug") {
+            isMinifyEnabled = false
+            (this as ExtensionAware).configure<com.google.firebase.crashlytics.buildtools.gradle.CrashlyticsExtension> {
+                mappingFileUploadEnabled = false
+            }
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.10")
+    implementation("androidx.core:core-ktx:1.8.0")
+
+    // Import the Firebase BoM
+    implementation(platform ("com.google.firebase:firebase-bom:30.3.2"))
+
+    // When using the BoM, don"t specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("com.google.firebase:firebase-crashlytics-ktx")
+    implementation("com.google.firebase:firebase-config-ktx")
+
+    // LeakCanary
+    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.9.1")
+
+    //binding by Kirill Rozov
+    implementation("com.github.kirich1409:viewbindingpropertydelegate:1.5.6")
+
+    //recycler
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+
+    //Room
+    implementation("androidx.room:room-runtime:2.4.3")
+    implementation("androidx.room:room-ktx:2.4.3")
+    kapt("androidx.room:room-compiler:2.4.3")
+
+    //Coil
+    implementation("io.coil-kt:coil:1.1.1")
+
+    //Lifecycle and ViewModel (архитектурные компоненты)
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    implementation("androidx.fragment:fragment-ktx:1.5.2")
+
+    //rxJava
+    implementation("io.reactivex.rxjava3:rxandroid:3.0.0")
+    implementation("io.reactivex.rxjava3:rxjava:3.1.3")
+    implementation("io.reactivex.rxjava3:rxkotlin:3.0.0")
+    implementation("com.jakewharton.rxrelay3:rxrelay:3.0.1")
+
+    //Okhttp
+    implementation("com.squareup.okhttp3:okhttp:4.9.2")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.2")
+
+    //retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:adapter-rxjava3:2.9.0")
+
+    //Cicerone
+    implementation("com.github.terrakok:cicerone:7.1")
+
+    //dagger
+    implementation("com.google.dagger:dagger:2.42")
+    kapt("com.google.dagger:dagger-compiler:2.42")
+
+    //UI
+    implementation("com.google.android.material:material:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    //AppCompat
+    implementation("androidx.appcompat:appcompat:1.5.0")
+
+
+    implementation("androidx.core:core-ktx:1.8.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+}
